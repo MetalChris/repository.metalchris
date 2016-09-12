91aa30a19d3eba5cbe14eba643ab9183c6310a43
MetalChris <cpesce@gmx.com>
2016-09-12 08:37:42 -0500

    Update ASN add Big South

--- plugin.video.bigsouth/addon.py
+++ plugin.video.bigsouth/addon.py
@@ -2,27 +2,29 @@
 #
 #
 # Written by MetalChris
-# Released under GPL(v2 or Later)
+# Released under GPL(v2) or Later
 
 import urllib, urllib2, xbmcplugin, xbmcaddon, xbmcgui, string, htmllib, os, platform, random, calendar, re, xbmcplugin, sys
 from bs4 import BeautifulSoup
 import HTMLParser
-import html5lib
-import requests
 import simplejson as json
-
+import html5lib
+from urllib import urlopen
+import requests
 
 _addon = xbmcaddon.Addon()
 _addon_path = _addon.getAddonInfo('path')
-selfAddon = xbmcaddon.Addon(id='plugin.video.asn')
+selfAddon = xbmcaddon.Addon(id='plugin.video.bigsouth')
 translation = selfAddon.getLocalizedString
 #usexbmc = selfAddon.getSetting('watchinxbmc')
 
-defaultimage = 'special://home/addons/plugin.video.asn/icon.png'
-defaultfanart = 'special://home/addons/plugin.video.asn/fanart.jpg'
-defaultvideo = 'special://home/addons/plugin.video.asn/icon.png'
-defaulticon = 'special://home/addons/plugin.video.asn/icon.png'
-baseurl = 'http://americansportsnet.com'
+defaultimage = 'special://home/addons/plugin.video.bigsouth/icon.png'
+defaultfanart = 'special://home/addons/plugin.video.bigsouth/fanart.jpg'
+defaultvideo = 'special://home/addons/plugin.video.bigsouth/icon.png'
+defaulticon = 'special://home/addons/plugin.video.bigsouth/icon.png'
+baseurl = 'http://video.nationalgeographic.com'
+live = 'aHR0cDovL2JpZ3NvdXRoc3BvcnRzLmNvbS9zZXJ2aWNlcy9hbGxhY2Nlc3MuYXNoeC8yL21lZGlhL2dldD90eXBlPUxpdmU='
+archive = 'aHR0cDovL2JpZ3NvdXRoc3BvcnRzLmNvbS9zZXJ2aWNlcy9hbGxhY2Nlc3MuYXNoeC8yL21lZGlhL2dldD90eXBlPUFyY2hpdmU='
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36'
 }
@@ -33,79 +35,46 @@ confluence_views = [500,501,502,503,504,508]
 
 
 def CATEGORIES():
-	addDir('Featured', 'http://www.americansportsnet.com/category/video/', 1, defaultimage)
-	addDir('Game Archive', 'http://americansportsnet.com/category/game-vault', 1, defaultimage)
-	addDir('Live Schedule', 'http://americansportsnet.com/schedule/', 3, defaultimage)
+	addDir('Live & Upcoming', live.decode('base64'), 1, defaultimage)
+	addDir('Video On Demand', archive.decode('base64'), 1, defaultimage)
 	xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
 
 #1
-def INDEX(url):
-	s = requests.Session()
-	r = s.get(url, headers=headers)
-	html = (r.text.encode('utf-8'))
-	soup = BeautifulSoup(html,'html5lib').find_all('div',{'class':'post-inner-content Video'})
-	for item in soup:
-	    title = item.find('h2').text.encode('utf-8')
-	    url = (item.find('a')['href'])
-	    image = item.find('img')['src']
-	    addDir2(title, url, 2, image)
-	next = BeautifulSoup(html,'html5lib').find_all('div',{'class':'nav-previous'})
-	next_url = re.compile('href="(.+?)"').findall(str(next))[-1]
-	addDir('Older Posts', next_url, mode, defaultimage)
-	xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-
-#2
-def IFRAME(name,url):
-        html = get_html(url)
-	soup = BeautifulSoup(html,'html5lib').find_all('div',{'class':'embed-container'})[-1]
-	iframe = re.compile('src="(.+?)"').findall(str(soup))[-1]
-	print iframe
-	if 'sinclair' in iframe:
-	    key = (iframe.rpartition('/')[-1]).replace('.html','')
-	    jurl = 'http://livevideostatus.sinclairstoryline.com/status/' + key
-	    print jurl
-            jgresponse = urllib2.urlopen(jurl)
-            jgdata = json.load(jgresponse)
-	    jkey = (jgdata['assetId'])
-	    jstuff = (jgdata['assetSignature'])
-	    channel = (jgdata['channelId'])
-	    purl = 'http://livevideostatus.sinclairstoryline.com/HLSproxy/variant/proxy.m3u8?manifestURL=http://content-ausw2.uplynk.com/' + jkey + '.m3u8?' + jstuff# + '&pbs=' + channel
-	    stream = 'http://content.uplynk.com/' + jkey + '.m3u8?' + jstuff# + '&pbs=' + channel
-	else:
-	    content = get_html(iframe)
-	    stream = re.compile("main_url = '(.+?)'").findall(str(content))[-1]
-	listitem = xbmcgui.ListItem(name, thumbnailImage = defaultimage)
-	xbmc.Player().play( stream, listitem )
-	sys.exit()
-	xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-
-#3
-def LIVE(url):
-        html = get_html(url)
-	soup = BeautifulSoup(html,'html5lib').find_all('tbody')[-1]
-	for item in soup:
-	    edate = str(re.compile('column-1">(.+?)</').findall(str(item)))[2:-2]
-	    if len(edate) <1:
-		continue
-	    sport = str(re.compile('column-2">(.+?)</').findall(str(item)))[2:-2]
-	    hteam = item.find('td', {'class':'column-3'}).text.strip()
-	    ateam = item.find('td', {'class':'column-4'}).text.strip()
-	    etime = str(re.compile('column-5">(.+?)</').findall(str(item)))[2:-2]
-	    link = str(re.compile('column-7">(.+?)</').findall(str(item)))[2:-2]
-	    if len(link) < 1:
-		continue
-	    if len(ateam) < 1:
-		title = edate + ' [' + etime + ']' + ' - ' + sport + ' - ' + hteam
+def INDEX(name,url):
+	response = get_html(url)
+	data = json.loads(response)#; i = 0
+        for i in range(30):
+	    title = (data['data'][i]['title'])
+	    sport_title = (data['data'][i]['sport_title'])
+	    date_utc = (data['data'][i]['date_utc']).replace('-','/').split('T')
+	    utc_date = (data['data'][i]['date_utc']).replace('T',' ')
+	    start = (date_utc[1]).split(':')
+	    etime = start[0] + ':' + start[1] + ' UTC'
+	    if 'Live' in name:
+		event = '[' + (date_utc[0])[5:]  + ' ' + etime + '] ' + title + ' (' + sport_title + ')'
 	    else:
-	        title = edate + ' [' + etime + ']' + ' - ' + sport + ' - ' + hteam + ' @ ' + ateam
-	    url = baseurl + '/' + link.replace('<!---{','').replace('}--->','')
-	    print title
-	    print url
-	    addDir2(title, url, 2, defaultimage)
-	xbmcplugin.endOfDirectory(int(sys.argv[1]))
+	        event = '[' + (date_utc[0])[5:] + '] ' + title + ' (' + sport_title + ')'
+	    url = (data['data'][i]['formats']['MobileH264'])
+	    if 'Demand' in name:
+	        r = requests.head(url)
+	        if r.status_code == 404:
+		    continue
+	    image = (data['data'][i]['poster'])
+            li = xbmcgui.ListItem(event, iconImage= image, thumbnailImage= image)
+	    li.addStreamInfo('video', { 'title': title })
+            xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, totalItems=30)
+            xbmcplugin.setContent(pluginhandle, 'episodes')
+        xbmcplugin.endOfDirectory(addon_handle, cacheToDisc=True)
+
+#99
+def PLAY(name,url):
+        html = get_html(url)
+	video = (re.compile('video src="(.+?)"').findall(html)[0]).replace('/z/','/i/') + '/master.m3u8'
+	print video
+	listitem = xbmcgui.ListItem(name, thumbnailImage = defaultimage)
+	xbmc.Player().play( video, listitem )
+	sys.exit()
+        xbmcplugin.endOfDirectory(addon_handle)
 
 
 def striphtml(data):
@@ -125,7 +94,9 @@ def sanitize(data):
 
 def get_html(url):
     req = urllib2.Request(url)
-    req.add_header('User-Agent','Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:47.0) Gecko/20100101 Firefox/47.0')
+    req.add_header('Host', 'bigsouthsports.com')
+    req.add_header('User-Agent','Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:48.0) Gecko/20100101 Firefox/48.0')
+    req.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
 
     try:
         response = urllib2.urlopen(req)
@@ -135,27 +106,6 @@ def get_html(url):
         response = False
         html = False
     return html
-
-
-def get_purl(url):
-    req = urllib2.Request(url)
-    #req.add_header('Host','livevideostatus.sinclairstoryline.com')
-    req.add_header('Referer','http://videobase.sinclairstoryline.com/jwplayer/jwplayer.flash.swf')
-    req.add_header('User-Agent','Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:47.0) Gecko/20100101 Firefox/47.0')
-    req.add_header('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
-    req.add_header('Accept-Language','en-US,en;q=0.5')
-    req.add_header('DNT','1')
-    req.add_header('Connection','keep-alive')
-
-    try:
-        response = urllib2.urlopen(req)
-        html = response.read()
-        response.close()
-    except urllib2.HTTPError:
-        response = False
-        html = False
-    return html
-
 
 def get_params():
     param = []
@@ -260,16 +210,16 @@ print "URL: " + str(url)
 print "Name: " + str(name)
 
 if mode == None or url == None or len(url) < 1:
-	print "American Sports Network Menu"
+	print "Big South Sports Menu"
 	CATEGORIES()
 elif mode == 1:
-	print "American Sports Network Videos"
-	INDEX(url)
+	print "Big South Sports Events"
+	INDEX(name,url)
 elif mode == 2:
-	print "American Sports Network Play Video"
-	IFRAME(name,url)
-elif mode == 3:
-	print "American Sports Network Live"
-	LIVE(url)
+	print "Big South Sports Sub Menu"
+	SUBCATEGORIES(url)
+elif mode == 99:
+	print "Play Video"
+	PLAY(name,url)
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
