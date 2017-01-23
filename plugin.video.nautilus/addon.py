8cbd80fd051fdd1d55a75862c37a57b2db973431
MetalChris <cpesce@gmx.com>
2017-01-23 09:33:11 -0600

    Update Nautilus Add Weather Unlocked

--- plugin.video.nautilus/addon.py
+++ plugin.video.nautilus/addon.py
@@ -4,11 +4,9 @@
 # Written by MetalChris
 # Released under GPL(v2 or Later)
 
-import urllib, urllib2, xbmcplugin, xbmcaddon, xbmcgui, string, htmllib, os, platform, random, calendar, re, xbmcplugin, sys
+import urllib, urllib2, xbmcplugin, xbmcaddon, xbmcgui, htmllib, os, platform, random, calendar, re, xbmcplugin, sys
 from bs4 import BeautifulSoup
-import HTMLParser
 import html5lib
-import requests
 import simplejson as json
 
 
@@ -31,24 +29,24 @@ defaultvideo = 'special://home/addons/plugin.video.nautilus/icon.png'
 defaulticon = 'special://home/addons/plugin.video.nautilus/icon.png'
 baseurl = 'http://nautiluslive.org'
 headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36'
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36'
 }
 
 pluginhandle = int(sys.argv[1])
 addon_handle = int(sys.argv[1])
 confluence_views = [500,501,502,503,504,508,510,514]
 window_id = xbmcgui.getCurrentWindowId()
-print window_id
+xbmc.log(str(window_id))
 
 
 def CATEGORIES():
 	if window_id == 10025:
-	    addDir('Channel 1', 'http://nautiluslive.org/live/channel-1', 1, defaultimage)
-	    addDir('Channel 2', 'http://nautiluslive.org/live/channel-2', 1, defaultimage)
-	    addDir('Quad', 'http://nautiluslive.org/live/quad', 1, defaultimage)
+		addDir('Channel 1', 'http://nautiluslive.org/live/channel-1', 1, defaultimage)
+		addDir('Channel 2', 'http://nautiluslive.org/live/channel-2', 1, defaultimage)
+		addDir('Quad', 'http://nautiluslive.org/live/quad', 1, defaultimage)
 	else:
-	    get_albums('http://nautiluslive.org/albums')
-	    #addDir('Photo Albums', 'http://nautiluslive.org/albums', 5, defaultimage)
+		get_albums('http://nautiluslive.org/albums')
+		#addDir('Photo Albums', 'http://nautiluslive.org/albums', 5, defaultimage)
 	xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
@@ -56,21 +54,21 @@ def CATEGORIES():
 def INDEX(name,url):
 	html = get_html(url)
 	try: soup = BeautifulSoup(html,'html5lib').find_all('iframe',{'id':'playerIFrame'})
-	except TypeError:                
-	    xbmcgui.Dialog().notification(plugin + ' ' +name, 'Expedition 2016 has ended.  Check back in April 2017.', defaultimage, 5000, False)
-	    sys.exit()
+	except TypeError:
+		xbmcgui.Dialog().notification(plugin + ' ' +name, 'Expedition 2016 has ended.  Check back in April 2017.', defaultimage, 5000, False)
+		sys.exit()
 	for item in soup:
-	    url = item.get('src')
-	    urlkey = re.compile('s=(.+?)&').findall(url)[-1]
-	    jsonurl = 'http://player.piksel.com/ws/get_live_series_events/s/' + urlkey + '/api/62c22d1d-269b-11e3-b5cd-005056865f49/apiv/4/mode/json'
-            jresponse = urllib2.urlopen(jsonurl)
-            jdata = json.load(jresponse)
-	    uuid = str(jdata['response']['getLiveSeriesEventsResponse']['liveEvents'])[3:11]
-	    streamurl = (jdata['response']['getLiveSeriesEventsResponse']['liveEvents'][uuid]['streamPackages'][0]['m3u8'])
-	    listitem = xbmcgui.ListItem('Nautilus Live ' + name, thumbnailImage = defaultimage)
-	    xbmc.Player().play( streamurl, listitem )
-	    sys.exit()
-	    xbmcplugin.endOfDirectory(int(sys.argv[1]))
+		url = item.get('src')
+		urlkey = re.compile('s=(.+?)&').findall(url)[-1]
+		jsonurl = 'http://player.piksel.com/ws/get_live_series_events/s/' + urlkey + '/api/62c22d1d-269b-11e3-b5cd-005056865f49/apiv/4/mode/json'
+		jresponse = urllib2.urlopen(jsonurl)
+		jdata = json.load(jresponse)
+		uuid = str(jdata['response']['getLiveSeriesEventsResponse']['liveEvents'])[3:11]
+		streamurl = (jdata['response']['getLiveSeriesEventsResponse']['liveEvents'][uuid]['streamPackages'][0]['m3u8'])
+		listitem = xbmcgui.ListItem('Nautilus Live ' + name, thumbnailImage = defaultimage)
+		xbmc.Player().play( streamurl, listitem )
+		sys.exit()
+		xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
 #5
@@ -78,146 +76,152 @@ def get_albums(url):
 	html = get_html(url)
 	soup = BeautifulSoup(html,'html5lib').find_all('div',{'class':'media-widget media-photo-click'})
 	for item in soup:
-	    title = item.find('h3').text
-	    url = baseurl + item.find('a')['href']
-	    image = re.compile('url\\((.+?)\\)').findall(str(item))[-1]
-            addDir(title, url, 10, image)
+		title = item.find('div',{'class':'title'}).text
+		url = baseurl + item.find('a')['href']
+		image = baseurl + item.find('img')['src']#re.compile('url\\((.+?)\\)').findall(str(item))[-1]
+		addDir(title, url, 10, image)
 	xbmcplugin.endOfDirectory(handle=addon_handle, succeeded=True, updateListing=False, cacheToDisc=True)
 
 
 #10
 def get_photos(name,url):
 	html = get_html(url)
-	soup = BeautifulSoup(html,'html5lib').find_all('div',{'class':'views-content-php'})
+	soup = BeautifulSoup(html,'html5lib').find_all('div',{'class':'grid-item'})
 	for item in soup:
-	    title = name
-	    image = baseurl + item.find('img')['src']
-	    url = unicode(image.replace('photo_thumbnail','photo_display_large'))
-	    url = ((url.split('itok'))[0])[:-1]
-	    liz=xbmcgui.ListItem(unicode(name), iconImage=unicode(image),thumbnailImage=unicode(image))
-	    liz.setInfo( type="Image", infoLabels={ "Title": name }) 
-            commands = []
-            commands.append
-            liz.addContextMenuItems([('Download Image', 'XBMC.RunPlugin(%s?mode=80&url=%s&name=%s)' % (sys.argv[0], url, title))])
-	    #xbmcplugin.setContent(addon_handle, 'picture')
-	    xbmcplugin.addDirectoryItem(handle=addon_handle,url=unicode(url),listitem=liz,isFolder=False)
+		if item.find('img')['alt']:
+			title = remove_crap(striphtml(str(item.find('img')['alt'])))
+		else:
+			title = remove_crap(name)
+		image = baseurl + item.find('img')['src']
+		url = unicode(image.replace('photo_thumbnail','photo_display_large'))
+		url = ((url.split('itok'))[0])[:-1]
+		liz=xbmcgui.ListItem(unicode(title), iconImage=unicode(image),thumbnailImage=unicode(image))
+		liz.setInfo( type="Image", infoLabels={ "Title": name })
+		#commands = []
+		#commands.append
+		#liz.addContextMenuItems([('Download Image', 'XBMC.RunPlugin(%s?mode=80&url=%s&name=%s)' % (sys.argv[0], url, title))])
+		#xbmcplugin.setContent(addon_handle, 'picture')
+		xbmcplugin.addDirectoryItem(handle=addon_handle,url=unicode(url),listitem=liz,isFolder=False)
 	#xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, liz)
-	print url
 	xbmcplugin.endOfDirectory(handle=addon_handle, succeeded=True, updateListing=False, cacheToDisc=True)
 
 
-def downloader(name,url):   
-        path = addon.getSetting('download')
-        if path == "":
-            xbmc.executebuiltin("XBMC.Notification(%s,%s,10000,%s)"
-                    %(translation(30000), translation(30010), defaulticon))
-            addon.openSettings()
-            path = addon.getSetting('download')
-        if path == "":
-            return
-        file_name = name + '-' + url.split('/')[-1]
-        dlsn = settings.getSetting(id="status")
-        bsize = settings.getSetting(id="bsize")
-        print 'Buffer Size= ' + str(bsize)
-        ret = xbmcgui.Dialog().yesno("Internet Archive [Video]", 'Download Selected File?', str(file_name))
-        if ret == False:
-            return get_photos
-        else:
-            xbmcgui.Dialog().notification('Nautilus Live', 'Download Started.', xbmcgui.NOTIFICATION_INFO, 5000)
-        print 'URL= ' + str(url)
-        print 'Filename= ' + str(file_name)
-        print 'Download Location= ' + str(download)
-	u = urllib2.urlopen(url)
-	f = open(download+file_name, 'wb')
-	meta = u.info()
-	file_size = float(meta.getheaders("Content-Length")[0])
-        file_sizeMB = float(file_size/(1024*1024))
-	print "Downloading: %s %s MB" % (file_name, "%.2f" % file_sizeMB)
-	file_size_dl = 0
-	block_sz = int(bsize)
-	while True:
-	    buffer = u.read(block_sz)
-	    if not buffer:
-	        break
-	    file_size_dl += float(len(buffer))
-            file_size_dlMB = float(file_size_dl/(1024*1024)) 
-	    f.write(buffer)
-	    status = "%.2f  [%3.2f%%]" % (file_size_dlMB, file_size_dl * 100. / file_size)
- 	    status = status + chr(8)*(len(status)+1)
-            if dlsn!='false':
-                xbmcgui.Dialog().notification('Nautilus Live Download in Progress', str(status) + ' of ' + ("%.2f" % float(file_sizeMB)) + ' MB', xbmcgui.NOTIFICATION_INFO, 2500)
-            else:
-                pass
-	f.close()
-        xbmcgui.Dialog().notification('Nautilus Live', 'Download Completed.', xbmcgui.NOTIFICATION_INFO, 5000)
-        print 'Download Completed'
+def downloader(name,url):
+		path = addon.getSetting('download')
+		if path == "":
+			xbmc.executebuiltin("XBMC.Notification(%s,%s,10000,%s)"
+					%(translation(30000), translation(30010), defaulticon))
+			addon.openSettings()
+			path = addon.getSetting('download')
+		if path == "":
+			return
+		file_name = url.split('/')[-1]
+		dlsn = settings.getSetting(id="status")
+		bsize = settings.getSetting(id="bsize")
+		xbmc.log( 'Buffer Size= ' + str(bsize))
+		ret = xbmcgui.Dialog().yesno("Nautilus Live", 'Download Selected File?', str(file_name))
+		if ret == False:
+			return get_photos
+		else:
+			xbmcgui.Dialog().notification('Nautilus Live', 'Download Started.', xbmcgui.NOTIFICATION_INFO, 5000)
+		xbmc.log( 'URL= ' + str(url))
+		xbmc.log( 'Filename= ' + str(file_name))
+		xbmc.log( 'Download Location= ' + str(download))
+		u = urllib2.urlopen(url)
+		f = open(download+file_name, 'wb')
+		meta = u.info()
+		file_size = float(meta.getheaders("Content-Length")[0])
+		file_sizeMB = float(file_size/(1024*1024))
+		xbmc.log( "Downloading: %s %s MB" % (file_name, "%.2f" % file_sizeMB))
+		file_size_dl = 0
+		block_sz = int(bsize)
+		while True:
+			buffer = u.read(block_sz)
+		if not buffer:
+			return
+		file_size_dl += float(len(buffer))
+		file_size_dlMB = float(file_size_dl/(1024*1024))
+		f.write(buffer)
+		status = "%.2f  [%3.2f%%]" % (file_size_dlMB, file_size_dl * 100. / file_size)
+		status = status + chr(8)*(len(status)+1)
+		if dlsn!='false':
+			xbmcgui.Dialog().notification('Nautilus Live Download in Progress', str(status) + ' of ' + ("%.2f" % float(file_sizeMB)) + ' MB', xbmcgui.NOTIFICATION_INFO, 2500)
+		else:
+			pass
+		f.close()
+		xbmcgui.Dialog().notification('Nautilus Live', 'Download Completed.', xbmcgui.NOTIFICATION_INFO, 5000)
+		xbmc.log( 'Download Completed')
 
+
+def remove_crap(data):
+	data = data.replace('&nbsp;',' ').replace('&amp;','&')
+	return data	
 
 
 def striphtml(data):
-    p = re.compile(r'<.*?>')
-    return p.sub('', data)
+	p = re.compile(r'<.*?>')
+	return p.sub('', data)
 
 
 def sanitize(data):
-    output = ''
-    for i in data:
-        for current in i:
-            if ((current >= '\x20') and (current <= '\xD7FF')) or ((current >= '\xE000') and (current <= '\xFFFD')) or ((current >= '\x10000') and (current <= '\x10FFFF')):
-               output = output + current
-    return output
+	output = ''
+	for i in data:
+		for current in i:
+			if ((current >= '\x20') and (current <= '\xD7FF')) or ((current >= '\xE000') and (current <= '\xFFFD')) or ((current >= '\x10000') and (current <= '\x10FFFF')):
+			   output = output + current
+	return output
 
 
 
 def get_html(url):
-    req = urllib2.Request(url)
-    req.add_header('User-Agent','Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:47.0) Gecko/20100101 Firefox/47.0')
+	req = urllib2.Request(url)
+	req.add_header('User-Agent','Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:47.0) Gecko/20100101 Firefox/47.0')
 
-    try:
-        response = urllib2.urlopen(req)
-        html = response.read()
-        response.close()
-    except urllib2.HTTPError:
-        response = False
-        html = False
-    return html
+	try:
+		response = urllib2.urlopen(req)
+		html = response.read()
+		response.close()
+	except urllib2.HTTPError:
+		response = False
+		html = False
+	return html
 
 
 def get_params():
-    param = []
-    paramstring = sys.argv[2]
-    if len(paramstring) >= 2:
-        params = sys.argv[2]
-        cleanedparams = params.replace('?', '')
-        if (params[len(params) - 1] == '/'):
-            params = params[0:len(params) - 2]
-        pairsofparams = cleanedparams.split('&')
-        param = {}
-        for i in range(len(pairsofparams)):
-            splitparams = {}
-            splitparams = pairsofparams[i].split('=')
-            if (len(splitparams)) == 2:
-                param[splitparams[0]] = splitparams[1]
+	param = []
+	paramstring = sys.argv[2]
+	if len(paramstring) >= 2:
+		params = sys.argv[2]
+		cleanedparams = params.replace('?', '')
+		if (params[len(params) - 1] == '/'):
+			params = params[0:len(params) - 2]
+		pairsofparams = cleanedparams.split('&')
+		param = {}
+		for i in range(len(pairsofparams)):
+			splitparams = {}
+			splitparams = pairsofparams[i].split('=')
+			if (len(splitparams)) == 2:
+				param[splitparams[0]] = splitparams[1]
 
-    return param
+	return param
 
 def addDir(name, url, mode, iconimage, fanart=False, infoLabels=True):
-    u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode) + "&name=" + urllib.quote_plus(name) + "&iconimage=" + urllib.quote_plus(iconimage)
-    ok = True
-    liz = xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-    liz.setInfo(type="Video", infoLabels={"Title": name})
-    liz.setProperty('IsPlayable', 'true')
-    if not fanart:
-        fanart=defaultfanart
-    liz.setProperty('fanart_image',fanart)
-    ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=True)
-    return ok
+	u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode) + "&name=" + urllib.quote_plus(name) + "&iconimage=" + urllib.quote_plus(iconimage)
+	ok = True
+	liz = xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+	liz.setInfo(type="Video", infoLabels={"Title": name})
+	liz.setProperty('IsPlayable', 'true')
+	if not fanart:
+		fanart=defaultfanart
+	liz.setProperty('fanart_image',fanart)
+	ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=True)
+	return ok
 
 def unescape(s):
-    p = htmllib.HTMLParser(None)
-    p.save_bgn()
-    p.feed(s)
-    return p.save_end()
+	p = htmllib.HTMLParser(None)
+	p.save_bgn()
+	p.feed(s)
+	return p.save_end()
 
 params = get_params()
 url = None
@@ -226,36 +230,36 @@ mode = None
 cookie = None
 
 try:
-    url = urllib.unquote_plus(params["url"])
+	url = urllib.unquote_plus(params["url"])
 except:
-    pass
+	pass
 try:
-    name = urllib.unquote_plus(params["name"])
+	name = urllib.unquote_plus(params["name"])
 except:
-    pass
+	pass
 try:
-    mode = int(params["mode"])
+	mode = int(params["mode"])
 except:
-    pass
+	pass
 
-print "Mode: " + str(mode)
-print "URL: " + str(url)
-print "Name: " + str(name)
+xbmc.log( "Mode: " + str(mode))
+xbmc.log( "URL: " + str(url))
+xbmc.log( "Name: " + str(name))
 
 if mode == None or url == None or len(url) < 1:
-	print "Nautilus Live Menu"
+	xbmc.log( "Nautilus Live Menu")
 	CATEGORIES()
 elif mode == 1:
-	print "Nautilus Live Play Video"
+	xbmc.log( "Nautilus Live Play Video")
 	INDEX(name,url)
 elif mode == 5:
-	print "Nautilus Photo Albums"
+	xbmc.log( "Nautilus Photo Albums")
 	get_albums(url)
 elif mode == 10:
-	print "Nautilus Photos"
+	xbmc.log( "Nautilus Photos")
 	get_photos(name,url)
 elif mode == 80:
-   print "Download File"
+   xbmc.log( "Download File")
    downloader(name,url)
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
---
+++ weather.unlocked/addon.py
@@ -0,0 +1,259 @@
+# *  This Program is free software; you can redistribute it and/or modify
+# *  it under the terms of the GNU General Public License as published by
+# *  the Free Software Foundation; either version 2, or (at your option)
+# *  any later version.
+# *
+# *  This Program is distributed in the hope that it will be useful,
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# *  GNU General Public License for more details.
+# *
+# *  You should have received a copy of the GNU General Public License
+# *  along with XBMC; see the file COPYING. If not, write to
+# *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+# *  http://www.gnu.org/copyleft/gpl.html
+
+import os, sys, socket, urllib2
+import xbmc, xbmcgui, xbmcaddon
+if sys.version_info < (2, 7):
+    import simplejson
+else:
+    import json as simplejson
+import time
+
+# This is a throwaway variable to deal with a python bug
+throwaway = time.strptime('20110101','%Y%m%d')
+
+__addon__      = xbmcaddon.Addon()
+__addonname__  = __addon__.getAddonInfo('name')
+__addonid__    = __addon__.getAddonInfo('id')
+__version__    = __addon__.getAddonInfo('version')
+__cwd__        = __addon__.getAddonInfo('path').decode("utf-8")
+__resource__   = xbmc.translatePath( os.path.join( __cwd__, 'resources', 'lib' ).encode("utf-8") ).decode("utf-8")
+
+sys.path.append(__resource__)
+
+from utilities import *
+
+LOC_URL          = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s'
+#LOC_QUERY        = 'http://maps.googleapis.com/maps/api/geocode/json?address="%s"'
+API_URL          = 'http://api.weatherunlocked.com/api/current/'
+APP_ID		= __addon__.getSetting(id="APP_ID")
+API_KEY	= __addon__.getSetting(id="API_KEY")
+#xbmc.log(str(APP_ID))
+#xbmc.log(str(API_KEY))
+WEATHER_ICON     = xbmc.translatePath('special://temp/weather/%s.png').decode("utf-8")
+WEATHER_WINDOW   = xbmcgui.Window(12600)
+NOW = time.time()
+LOCAL_TIME = time.localtime(NOW)
+HOUR = time.strftime("%H", LOCAL_TIME)
+TODAY = time.strftime("%A", LOCAL_TIME)
+##xbmc.log('LOCAL TIME: ' + str(LOCAL_TIME))
+#xbmc.log('HOUR: ' + str(HOUR))
+
+socket.setdefaulttimeout(10)
+
+def log(txt):
+    if isinstance (txt,str):
+        txt = txt.decode("utf-8")
+    message = u'%s: %s' % (__addonid__, txt)
+    #xbmc.log(msg=message.encode("utf-8"), level=#xbmc.logDEBUG)
+
+def set_property(name, value):
+    WEATHER_WINDOW.setProperty(name, value)
+
+def refresh_locations():
+    locations = 0
+    for count in range(1, 4):
+        loc_name = __addon__.getSetting('Location%s' % count)
+        if loc_name != '':
+            locations += 1
+        set_property('Location%s' % count, loc_name)
+    set_property('Locations', str(locations))
+    log('available locations: %s' % str(locations))
+
+def location(loc):
+    items  = []
+    locs   = []
+    locids = []
+    query = find_location(loc)
+    data = parse_data(query)
+    listitem   = data['results'][0]['formatted_address']
+    location = listitem
+    lat = data['results'][0]['geometry']['location']['lat']
+    lng = data['results'][0]['geometry']['location']['lng']
+    locationid = str(lat) + ',' + str(lng)
+    items.append(listitem)
+    locs.append(location)
+    locids.append(locationid)
+    return items, locs, locids
+
+def find_location(loc):
+    query = urllib2.quote(loc)
+    url = LOC_URL % query
+    try:
+        req = urllib2.urlopen(url)
+        response = req.read()
+        req.close()
+    except:
+        response = ''
+    return response
+
+def parse_data(reply):
+    try:
+        data = simplejson.loads(reply)
+    except:
+        log('failed to parse weather data')
+        data = ''
+    return data
+
+def forecast(loc,locid):
+    log('weather location: %s' % locid)
+    retry = 0
+    while (retry < 6) and (not xbmc.abortRequested):
+        query = get_weather(locid)
+        if query != '':
+            retry = 6
+        else:
+            retry += 1
+            xbmc.sleep(10000)
+            log('weather download failed')
+    log('forecast data: %s' % query)
+    if query != '':
+        properties(query,loc,locid)
+    else:
+        clear()
+
+def get_weather(locid):
+    xbmc.log('GET WEATHER')
+    url = API_URL + str(locid) + '?app_id=' + APP_ID + '&app_key=' + API_KEY
+    try:
+        req = urllib2.Request(url)
+        req.add_header('User-Agent','Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:47.0) Gecko/20100101 Firefox/47.0')
+        req.add_header('Accept','application/json')
+        response = urllib2.urlopen(req)
+        data = response.read()
+        req.close()
+    except:
+        response = ''
+    return data
+
+def get_forecast(locid):
+    xbmc.log('GET FORECAST')
+    url = API_URL.replace('current','forecast') + str(locid) + '?app_id=' + APP_ID + '&app_key=' + API_KEY
+    try:
+        req = urllib2.Request(url)
+        req.add_header('User-Agent','Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:47.0) Gecko/20100101 Firefox/47.0')
+        req.add_header('Accept','application/json')
+        response = urllib2.urlopen(req)
+        data = response.read()
+        req.close()
+    except:
+        response = ''
+    return data
+
+def clear():
+    set_property('Current.Condition'     , 'N/A')
+    set_property('Current.Temperature'   , '0')
+    set_property('Current.Wind'          , '0')
+    set_property('Current.WindDirection' , 'N/A')
+    set_property('Current.Humidity'      , '0')
+    set_property('Current.FeelsLike'     , '0')
+    set_property('Current.UVIndex'       , '0')
+    set_property('Current.DewPoint'      , '0')
+    set_property('Current.OutlookIcon'   , 'na.png')
+    set_property('Current.FanartCode'    , 'na')
+    for count in range (0, 5):
+        set_property('Day%i.Title'       % count, 'N/A')
+        set_property('Day%i.HighTemp'    % count, '0')
+        set_property('Day%i.LowTemp'     % count, '0')
+        set_property('Day%i.Outlook'     % count, 'N/A')
+        set_property('Day%i.OutlookIcon' % count, 'na.png')
+        set_property('Day%i.FanartCode'  % count, 'na')
+
+def properties(data,loc,locid):
+    xbmc.log('PROPERTIES')
+    wdata = parse_data(data)
+    weathercode = WEATHER_CODES[(wdata['wx_icon']).lower().replace('.gif','')]
+    set_property('Current.Location'      , loc)
+    set_property('Current.Condition'     , wdata['wx_desc'])
+    set_property('Current.Temperature'   , str(wdata['temp_c']))
+    set_property('Current.Wind'          , str(wdata['windspd_mph']))
+    set_property('Current.WindDirection' , str(wdata['winddir_compass']))
+    set_property('Current.WindChill'     , str(wdata['feelslike_c']))
+    set_property('Current.Humidity'      , str(wdata['humid_pct']))
+    set_property('Current.Visibility'    , str(wdata['vis_mi']))
+    set_property('Current.Pressure'      , str(wdata['slp_in']))
+    set_property('Current.FeelsLike'     , str(wdata['feelslike_c']))#wdata['currentobservation']['WindChill'])
+    set_property('Current.DewPoint'      , str(wdata['dewpoint_c']))
+    set_property('Current.UVIndex'       , 'N/A')
+    set_property('Current.OutlookIcon'   , '%s.png' % weathercode)
+    #set_property('Current.FanartCode'    , ''#condition[0].attributes['code'].value)
+    forecast = get_forecast(locid)
+    #xml = minidom.parseString(str(forecast))
+    #forecast = xml.getElementsByTagName('Day')
+    ##xbmc.log(str(forecast)[0:500])
+    forecast = parse_data(forecast)
+    set_property('Today.Sunrise'         , str(forecast['Days'][0]['sunrise_time']))
+    set_property('Today.Sunset'          , str(forecast['Days'][0]['sunset_time']))
+    #xbmc.log('HOUR: ' + str(HOUR))
+    if HOUR < 12:
+	count = 1
+    else:
+        count = 0
+    for count in range(0,5):
+	#xbmc.log(str(count))
+	#xbmc.log(str(TODAY))
+        set_property('Day%i.HighTemp'    % count, str(forecast['Days'][count+1]['temp_max_c']))
+        set_property('Day%i.LowTemp'     % count, str(forecast['Days'][count+1]['temp_min_c']))
+       # set_property('Day%i.Title'       % count, str(forecast['Days'][count+1]['date']))
+	fdate = str(forecast['Days'][count+1]['date'])
+	fday = time.strptime(fdate, "%d/%m/%Y")#.strftime("%m/%d/%Y")
+	fdaynew = time.strftime("%A", fday)
+	#xbmc.log(str(fdaynew))
+	if str(fdaynew) != str(TODAY):
+            set_property('Day%i.Title'       % count, str(fdaynew))
+	else:
+	    set_property('Day%i.Title'       % count, 'Today')
+        set_property('Day%i.Outlook'     % count, str(forecast['Days'][count+1]['Timeframes'][3]['wx_desc']))
+        ##set_property('Day%i.FanartCode'  , '20')
+        weathercode = WEATHER_CODES[str(forecast['Days'][count+1]['Timeframes'][3]['wx_icon']).lower().replace('.gif','')]
+        set_property('Day%i.OutlookIcon' % count, '%s.png' % weathercode)
+	#'special://home/addons/weather.unlocked/resources/media/'
+	count = count + 1
+	##xbmc.log(str(count))
+#log('version %s started: %s' % (__version__, sys.argv))
+
+set_property('WeatherProvider', __addonname__)
+set_property('WeatherProviderLogo', xbmc.translatePath(os.path.join(__cwd__, 'resources', 'banner.png')))
+
+if sys.argv[1].startswith('Location'):
+    keyboard = xbmc.Keyboard('', xbmc.getLocalizedString(14024), False)
+    keyboard.doModal()
+    if (keyboard.isConfirmed() and keyboard.getText() != ''):
+        text = keyboard.getText()
+        items, locs, locids = location(text)
+        dialog = xbmcgui.Dialog()
+        if locs != []:
+            selected = dialog.select(xbmc.getLocalizedString(396), items)
+            if selected != -1:
+                __addon__.setSetting(sys.argv[1], locs[selected])
+                __addon__.setSetting(sys.argv[1] + 'id', locids[selected])
+                log('selected location: %s' % locs[selected])
+        else:
+            dialog.ok(__addonname__, xbmc.getLocalizedString(284))
+else:
+    location = __addon__.getSetting('Location%s' % sys.argv[1])
+    locationid = __addon__.getSetting('Location%sid' % sys.argv[1])
+    if (locationid == '') and (sys.argv[1] != '1'):
+        location = __addon__.getSetting('Location1')
+        locationid = __addon__.getSetting('Location1id')
+        log('trying location 1 instead')
+    if not locationid == '':
+        forecast(location, locationid)
+    else:
+        log('no location found')
+        clear()
+    refresh_locations()
+
+log('finished')
