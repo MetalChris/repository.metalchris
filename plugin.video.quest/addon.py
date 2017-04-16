8c460d2efd9fb079fb9f02cf396d317a504ebf5e
MetalChris <cpesce@gmx.com>
2017-04-16 18:37:59 -0500

    Update Quest TV

--- plugin.video.quest/addon.py
+++ plugin.video.quest/addon.py
@@ -33,6 +33,59 @@ addon_handle = int(sys.argv[1])
 confluence_views = [500,501,502,503,504,508,515]
 
 
+#5
+def menu():
+	addDir('All Shows','http://www.questtv.co.uk/video/',6,defaulticon)
+	html = get_html('http://www.questtv.co.uk/video/')
+	soup = BeautifulSoup(html,'html5lib').find_all("section",{"class":"other_playlists"})
+	links = BeautifulSoup(str(soup),'html5lib').find_all('li')
+	for name in links:
+		data = name.find('a')['data']
+		#xbmc.log('DATA: ' + str(data))
+		name = name.find('p').text
+		addDir(name,data,7,defaulticon)
+	xbmcplugin.endOfDirectory(addon_handle)
+
+#6
+def folders():
+	html = get_html('http://www.questtv.co.uk/video/')
+	soup = BeautifulSoup(html,'html5lib').find_all("div",{"class":"dni-video-playlist-thumb-box"})
+	xbmc.log('SOUP: ' + str(len(soup)))
+	mode = 53
+	make_list(url,mode,soup)
+	xbmcplugin.endOfDirectory(addon_handle)
+
+#7
+def show_folders(url):
+	html = get_html('http://www.questtv.co.uk/video/')
+	section = BeautifulSoup(html,'html5lib').find_all("div",{"data":url})
+	soup = BeautifulSoup(str(section),'html5lib').find_all("div",{"class":"dni-video-playlist-thumb-box"})
+	xbmc.log('SOUP: ' + str(len(soup)))
+	mode = 54
+	make_list(url,mode,soup)
+	xbmcplugin.endOfDirectory(addon_handle)
+
+
+def make_list(url,mode,data):
+	shows = []
+	for show in data:
+		show = show.find('h3').text.encode('utf-8').split(':')[0].split(' - ')[0].rstrip('1234567890 ')
+		shows.append(show)
+		shows_set = set(shows)
+		shows = list(shows_set)
+	#xbmc.log('SHOWS: ' + str(shows))
+	list_shows(url,mode,shows)
+	xbmcplugin.endOfDirectory(addon_handle)
+
+
+def list_shows(url,mode,data):
+	for title in data:
+		add_directory(title, url, mode, defaultfanart, defaultimage, plot='')
+		xbmcplugin.setContent(addon_handle, 'episodes')
+		xbmcplugin.addSortMethod(addon_handle, xbmcplugin.SORT_METHOD_EPISODE)
+	xbmcplugin.endOfDirectory(addon_handle)
+
+
 #10
 def stream(name,url):
 	listitem = xbmcgui.ListItem(name, thumbnailImage=defaultimage)
@@ -41,21 +94,40 @@ def stream(name,url):
 
 
 #53
-def quest_shows():
+def quest_shows(name,url):
 	html = get_html('http://www.questtv.co.uk/video/')
 	soup = BeautifulSoup(html,'html5lib').find_all("div",{"class":"dni-video-playlist-thumb-box"})
 	xbmc.log('SOUP: ' + str(len(soup)))
 	for title in soup:
-		#if title.find('a'):
-			#url = title.find('a')['href']
-		#else:
-			#continue
 		image = title.find('img')['src']
 		plot = title.find('p').text
 		videoid = title.find('div')['data-videoid']
-		stream = 'http://c.brightcove.com/services/mobile/streaming/index/master.m3u8?videoId=' + str(videoid) + '&pubId=4401740954001'
+		stream = 'http://c.brightcove.com/services/mobile/streaming/index/master.m3u8?videoId=' + str(videoid)# + '&pubId=4401740954001'
 		title = title.find('h3').text.encode('utf-8')
-		add_directory2(title, stream, 10, defaultfanart, image, plot=plot)
+		if not name in title:
+			continue
+		add_directory(title, stream, 10, defaultfanart, image, plot=plot)
+	xbmcplugin.setContent(addon_handle, 'episodes')
+	xbmcplugin.addSortMethod(addon_handle, xbmcplugin.SORT_METHOD_EPISODE)
+	xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+
+
+#54
+def shows(url):
+	html = get_html('http://www.questtv.co.uk/video/')
+	section = BeautifulSoup(html,'html5lib').find_all("div",{"data":url})
+	soup = BeautifulSoup(str(section),'html5lib').find_all("div",{"class":"dni-video-playlist-thumb-box"})
+	xbmc.log('SOUP: ' + str(len(soup)))
+	for title in soup:
+		image = title.find('img')['src']
+		plot = title.find('p').text
+		videoid = title.find('div')['data-videoid']
+		stream = 'http://c.brightcove.com/services/mobile/streaming/index/master.m3u8?videoId=' + str(videoid)# + '&pubId=4401740954001'
+		title = title.find('h3').text.encode('utf-8')
+		if not name in title:
+			continue
+		add_directory(title, stream, 10, defaultfanart, image, plot=plot)
 	xbmcplugin.setContent(addon_handle, 'episodes')
 	xbmcplugin.addSortMethod(addon_handle, xbmcplugin.SORT_METHOD_EPISODE)
 	xbmcplugin.endOfDirectory(int(sys.argv[1]))
@@ -78,17 +150,29 @@ def striphtml(data):
 	return p.sub('', data)
 
 
-def add_directory2(name,url,mode,fanart,thumbnail,plot):
-		u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
-		ok=True
-		liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=thumbnail)
-		liz.setInfo( type="Video", infoLabels={ "Title": name,
-												"plot": plot} )
-		if not fanart:
-			fanart=''
-		liz.setProperty('fanart_image',fanart)
-		ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True, totalItems=10)
-		return ok
+def add_directory(name,url,mode,fanart,thumbnail,plot):
+	u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)#+"&thumbnail="+urllib.unquote(thumbnail)
+	ok=True
+	liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=thumbnail)
+	liz.setInfo( type="Video", infoLabels={ "Title": name,
+											"plot": plot} )
+	if not fanart:
+		fanart=''
+	liz.setProperty('fanart_image',fanart)
+	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True, totalItems=10)
+	return ok
+
+def addDir(name, url, mode, iconimage, fanart=False, infoLabels=True):
+	u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode) + "&name=" + urllib.quote_plus(name)# + "&iconimage=" + urllib.quote_plus(iconimage)
+	ok = True
+	liz = xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+	liz.setInfo(type="Video", infoLabels={"Title": name})
+	liz.setProperty('IsPlayable', 'true')
+	if not fanart:
+		fanart=defaultfanart
+	liz.setProperty('fanart_image',fanart)
+	ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=True)
+	return ok
 
 def get_html(url):
 	req = urllib2.Request(url)
@@ -102,24 +186,6 @@ def get_html(url):
 		response = False
 		html = False
 	return html
-
-def get_json(url):
-	req = urllib2.Request(url)
-	req.add_header('Host', 'edge.api.brightcove.com')
-	req.add_header('User-Agent','Roku/DVP-4.3 (024.03E01057A), Mozilla/5.0 (iPad; CPU OS 8_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12B410 Safari/600.1.4')
-	req.add_header('Accept', 'application/json;pk=BCpkADawqM0dhxjC63Ux5MXyiMyIYB1S1bvk0iorISSaD1jFgWDyiv-JAcvE6XduNdDYxMdk_NTQWn91IQI9NLPkXd5UIw3cv49pcyJ5eW9QT0CWTrclSFHBHqSSyJ_9Ysgzc2v-Mw0wxNmZ')
-
-	try:
-		response = urllib2.urlopen(req)
-		json = response.read()
-		response.close()
-	except urllib2.HTTPError:
-		response = False
-		json = False
-	return json
-
-
-
 
 def get_params():
 	param = []
@@ -146,8 +212,6 @@ def unescape(s):
 	return p.save_end()
 
 
-
-
 params = get_params()
 url = None
 name = None
@@ -166,6 +230,10 @@ try:
 	mode = int(params["mode"])
 except:
 	pass
+try:
+	thumbnail = urllib.unquote(params["thumbnail"])
+except:
+	pass
 
 print "Mode: " + str(mode)
 print "URL: " + str(url)
@@ -173,17 +241,29 @@ print "Name: " + str(name)
 
 if mode == None or url == None or len(url) < 1:
 	print "Generate Main Menu"
-	quest_shows()
+	menu()
 elif mode == 1:
 	print "Indexing Videos"
 	INDEX(url)
 elif mode == 4:
 	print "Play Video"
+elif mode == 5:
+	print "Get Quest Shows"
+	menu()
+elif mode == 6:
+	print "Get Quest Show Titles"
+	folders()
+elif mode == 7:
+	print "Get Quest Show Titles"
+	show_folders(url)
 elif mode == 10:
 	print "Get Quest Stream"
 	stream(name,url)
 elif mode == 53:
 	print "Quest Shows"
-	cfl_2016(url)
+	quest_shows(name,url)
+elif mode == 54:
+	print "Quest Shows"
+	shows(url)
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
