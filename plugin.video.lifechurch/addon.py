4499709d61764ed4a923e5e6205fd9ee809e1ddb
MetalChris <cpesce@gmx.com>
2017-06-18 09:52:42 -0500

    Update Life Church

--- plugin.video.lifechurch/addon.py
+++ plugin.video.lifechurch/addon.py
@@ -6,6 +6,7 @@
 
 import urllib, urllib2, xbmcplugin, xbmcaddon, xbmcgui, htmllib, re, sys
 from bs4 import BeautifulSoup
+import simplejson as json
 
 
 artbase = 'special://home/addons/plugin.video.lifechurch/resources/media/'
@@ -54,8 +55,33 @@ def podcast():
 	xbmcplugin.endOfDirectory(addon_handle)
 
 
+#631
+def live():
+	response = get_html('https://lifechurch-tv.churchonline.org/api/v1/events/current')
+	jdata = json.loads(response)
+	live_key = (jdata['response']['item']['isLive'])
+	print live_key
+	if str(live_key) != 'False':
+		stream = 'https://churchonline-streaming.life.church/fastly/ngrp:lifechurch_adaptive/playlist.m3u8'
+		listitem = xbmcgui.ListItem(plugin + ' ' + name, thumbnailImage=defaulticon)
+		listitem.setProperty('mimetype', 'video/x-mpegurl')
+		xbmc.Player().play( stream, listitem )
+		sys.exit()
+	else:
+		line1 = "No Live Broadcast Available"
+		line2 = "M-F: Every 90 Minutes Starting 7 AM CT"
+		line3 = "5:30 PM Sat Thru 10 PM Sun"
+		xbmcgui.Dialog().ok(plugin, line1, line2, line3)
+		#xbmcgui.Dialog().notification(plugin, 'No Live Broadcast Available.', defaultimage, 5000, False)
+		sys.exit()
+		print 'No Live Broadcast Available'
+		return
+		xbmcplugin.endOfDirectory(addon_handle)
+
+
 #633
 def shows():
+	add_directory2('Live', 'https://lifechurch-tv.churchonline.org/api/v1/events/current', 631, defaultfanart, defaultimage, plot='')
 	add_directory2('PodCast', 'http://feedpress.me/lifechurchipod', 632, defaultfanart, defaultimage, plot='')
 	html = get_html('https://www.life.church/watch/messages/')
 	soup = BeautifulSoup(html,'html.parser').find_all("div",{"class":"card messages-card"})
@@ -136,7 +162,7 @@ def get_redirected_url(url):
 	opener = urllib2.build_opener(urllib2.HTTPRedirectHandler)
 	request = opener.open(url)
 	#print 'Redirect' + str(request.url)
-	playthis = request.url
+	#playthis = request.url
 	#play(playthis)
 	return request.url
 
@@ -203,49 +229,6 @@ def get_params():
 
 	return param
 
-def addListItem(label, image, url, isFolder, infoLabels = False, fanart = False, duration = False):
-	listitem = xbmcgui.ListItem(label = label, iconImage = image, thumbnailImage = image)
-	if not isFolder:
-		if settings.getSetting('download') == '' or settings.getSetting('download') == 'false':
-			listitem.setProperty('IsPlayable', 'true')
-	if fanart:
-		listitem.setProperty('fanart_image', fanart)
-	if infoLabels:
-		listitem.setInfo(type = 'video', infoLabels = infoLabels)
-		if duration:
-			if hasattr(listitem, 'addStreamInfo'):
-				listitem.addStreamInfo('video', { 'duration': int(duration) })
-			else:
-				listitem.setInfo(type = 'video', infoLabels = { 'duration': str(datetime.timedelta(milliseconds=int(duration)*1000)) } )
-	ok = xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = u, listitem = listitem, isFolder = isFolder)
-	return ok
-
-def addLink(name, url, mode, iconimage, fanart=False, infoLabels=True):
-	u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode) + "&name=" + urllib.quote_plus(name) + "&iconimage=" + urllib.quote_plus(iconimage)
-	ok = True
-	liz = xbmcgui.ListItem(name, iconImage=iconimage, thumbnailImage=iconimage)
-	liz.setInfo(type="Video", infoLabels={"Title": name})
-	liz.setProperty('IsPlayable', 'true')
-	if not fanart:
-		fanart=defaultfanart
-	liz.setProperty('fanart_image',fanart)
-	ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz,isFolder=False)
-	return ok
-
-def add_item( action="" , title="" , plot="" , url="" ,thumbnail="" , folder=True ):
-	_log("add_item action=["+action+"] title=["+title+"] url=["+url+"] thumbnail=["+thumbnail+"] folder=["+str(folder)+"]")
-
-	listitem = xbmcgui.ListItem( title, iconImage=iconimage, thumbnailImage=iconimage )
-	listitem.setInfo( "video", { "Title" : title, "FileName" : title, "Plot" : plot } )
-
-	if url.startswith("plugin://"):
-		itemurl = url
-		listitem.setProperty('IsPlayable', 'true')
-		xbmcplugin.addDirectoryItem( handle=int(sys.argv[1]), url=itemurl, listitem=listitem)
-	else:
-		itemurl = '%s?action=%s&title=%s&url=%s&thumbnail=%s&plot=%s' % ( sys.argv[ 0 ] , action , urllib.quote_plus( title ) , urllib.quote_plus(url) , urllib.quote_plus( thumbnail ) , urllib.quote_plus( plot ))
-		xbmcplugin.addDirectoryItem( handle=int(sys.argv[1]), url=itemurl, listitem=listitem, isFolder=folder)
-		return ok
 
 def addDir(name, url, mode, iconimage, fanart=False, infoLabels=True):
 	u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode) + "&name=" + urllib.quote_plus(name) + "&iconimage=" + urllib.quote_plus(iconimage)
@@ -270,14 +253,6 @@ def addDir2(name,url,mode,iconimage, fanart=False, infoLabels=False):
 		liz.setProperty('fanart_image',fanart)
 		ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
 		return ok
-
-
-def addDirectoryItem2(name, isFolder=True, parameters={}):
-	''' Add a list item to the XBMC UI.'''
-	li = xbmcgui.ListItem(name, iconImage=defaultimage, thumbnailImage=defaultimage)
-	li.setProperty('fanart_image', defaultfanart)
-	url = sys.argv[0] + '?' + urllib.urlencode(parameters)
-	return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=li, isFolder=isFolder)
 
 
 def unescape(s):
@@ -322,6 +297,9 @@ if mode == None or url == None or len(url) < 1:
 	shows()
 elif mode == 4:
 	print "Play Video"
+elif mode==631:
+	print "Life Church Live"
+	live()
 elif mode==632:
 	print "Life Church PodCast"
 	podcast()
