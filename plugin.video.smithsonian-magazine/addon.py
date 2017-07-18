9c0c4a1adc6ff3480987e8cee865523ad8502da8
MetalChris <cpesce@gmx.com>
2017-07-18 17:20:17 -0500

    Update Smithsonian Magazine

--- plugin.video.smithsonian-magazine/addon.py
+++ plugin.video.smithsonian-magazine/addon.py
@@ -5,11 +5,14 @@
 # Released under GPL(v2) or Later
 
 import urllib, urllib2, xbmcplugin, xbmcaddon, xbmcgui, string, htmllib, os, platform, re, xbmcplugin, sys
-import requests  
+import requests
 import urlparse
 import HTMLParser
 from bs4 import BeautifulSoup
 from urllib import urlopen
+import simplejson as json
+#import base64
+## a.decode('base64') ##
 
 
 artbase = 'special://home/addons/plugin.video.smithsonian-magazine/resources/media/'
@@ -44,14 +47,14 @@ def index():
 	soup = BeautifulSoup(response,'html.parser')
 	match = re.compile('value="(.+?)">(.+?)</option').findall(str(soup))
 	for link, title in match[0:13]:
-	    title = title.encode('utf-8').replace('&amp;','&')
-	    url = baseurl + link + '?no-ist'
-	    thumbnail = defaultimage
-            addDir2(title, url, 636, defaulticon, defaultfanart)
-        views = settings.getSetting(id="views")
+		title = title.encode('utf-8').replace('&amp;','&')
+		url = baseurl + link + '?no-ist'
+		thumbnail = defaultimage
+		addDir2(title, url, 636, defaulticon, defaultfanart)
+	views = settings.getSetting(id="views")
 	if views != 'false':
-            xbmc.executebuiltin("Container.SetViewMode("+str(confluence_views[6])+")")
-        xbmcplugin.endOfDirectory(addon_handle)
+			xbmc.executebuiltin("Container.SetViewMode("+str(confluence_views[6])+")")
+	xbmcplugin.endOfDirectory(addon_handle)
 
 
 #636
@@ -59,98 +62,95 @@ def videos(url):
 	response = get_html(url)
 	next = BeautifulSoup(response,'html.parser').find_all('a',{'class':'next pager'})
 	if len(next) > 0:
-	    nextp = str(re.compile('href="(.+?)">').findall(str(next))[-1])
-	    nextpage = (url.rsplit('/',1))[0] + '/' + nextp
+		nextp = str(re.compile('href="(.+?)">').findall(str(next))[-1])
+		nextpage = (url.rsplit('/',1))[0] + '/' + nextp
 	soup = BeautifulSoup(response,'html.parser').find_all('div',{'class':'teaser-list'})
 	items = BeautifulSoup(str(soup),'html.parser').find_all('a',{'class':'media-teaser '})
 	for item in items:
-	    title = item.find('h5').text.strip()
-	    duration = title.split('   ')[-1]
-	    title = remove_non_ascii_1(title).split('   ')[0]
-	    image = item.find('img')['src']
-	    key = re.compile('data-api_id="(.+?)"').findall(str(item))[-1]
-	    if QUALITY =='1':
-	        sub = '_1500.m3u8'
-	    elif QUALITY =='0':
-	        sub = '_900.m3u8'
-	    else:
-	        sub = '.m3u8'		
-	    url = 'http://player.ooyala.com/player/all/' + key + sub
-	    description = item.find('span',{'class':'description hidden'}).text + ' ' + duration
-            infoLabels = {'title':title, 'plot':description}
-            li = xbmcgui.ListItem(title, iconImage= image, thumbnailImage= image)
-            li.setProperty('fanart_image',  image)
-            li.setProperty('mimetype', 'video/mp4')
-            li.setInfo( type='Video', infoLabels=infoLabels )  
-            xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, totalItems=10)
+		title = item.find('h5').text.strip()
+		duration = title.split('   ')[-1]
+		title = remove_non_ascii_1(title).split('   ')[0]
+		image = item.find('img')['src']
+		url = 'http://www.smithsonianmag.com' + re.compile('href="(.+?)"').findall(str(item))[0]
+		description = item.find('span',{'class':'description hidden'}).text + ' ' + duration
+		add_directory2(title,url,638,image,image,plot=description)
 	if len(next) > 0:
-            addDir2('Next Page', nextpage, 636, defaulticon, defaultfanart)
-        xbmcplugin.setContent(pluginhandle, 'episodes')
-        views = settings.getSetting(id="views")
+			addDir2('Next Page', nextpage, 636, defaulticon, defaultfanart)
+	xbmcplugin.setContent(pluginhandle, 'episodes')
+	views = settings.getSetting(id="views")
 	if views != 'false':
-            xbmc.executebuiltin("Container.SetViewMode("+str(confluence_views[3])+")")
-        xbmcplugin.endOfDirectory(addon_handle)
+		xbmc.executebuiltin("Container.SetViewMode("+str(confluence_views[3])+")")
+	xbmcplugin.endOfDirectory(addon_handle)
+
+
+#638
+def streams(url):
+	response = get_html(url)
+	key = re.compile('mediaid": "(.+?)"').findall(str(response))[-1]
+	url = 'https://cdn.jwplayer.com/manifests/' + key + '.m3u8'
+	play(url)
 
 
 def striphtml(data):
-    p = re.compile(r'<.*?>')
-    return p.sub('', data)
+	p = re.compile(r'<.*?>')
+	return p.sub('', data)
 
 
 def play(url):
-    item = xbmcgui.ListItem(path=url)
-    return xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
+	item = xbmcgui.ListItem(path=url)
+	return xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
 
 
 def remove_non_ascii_1(text):
-    return ''.join(i for i in text if ord(i)<128)
+	return ''.join(i for i in text if ord(i)<128)
 
 
 def add_directory2(name,url,mode,fanart,thumbnail,plot):
-        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
-        ok=True
-        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=thumbnail)
-        liz.setInfo( type="Video", infoLabels={ "Title": name,
-                                                "plot": plot} )
-        if not fanart:
-            fanart=''
-        liz.setProperty('fanart_image',fanart)
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True, totalItems=40)
-        return ok
+	u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+	ok=True
+	liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=thumbnail)
+	liz.setInfo( type="Video", infoLabels={ "Title": name,
+											"plot": plot} )
+	liz.setProperty('IsPlayable', 'true')
+	if not fanart:
+		fanart=''
+	liz.setProperty('fanart_image',fanart)
+	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False, totalItems=40)
+	return ok
 
 def get_html(url):
-    req = urllib2.Request(url)
-    req.add_header('Host', 'www.smithsonianmag.com')
-    req.add_header('User-Agent','User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:44.0) Gecko/20100101 Firefox/44.0')
-    req.add_header('Referer', 'http://www.smithsonianmag.com/videos/')
-    req.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
+	req = urllib2.Request(url)
+	req.add_header('Host', 'www.smithsonianmag.com')
+	req.add_header('User-Agent','User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:44.0) Gecko/20100101 Firefox/44.0')
+	req.add_header('Referer', 'http://www.smithsonianmag.com/videos/')
+	req.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
 
-    try:
-        response = urllib2.urlopen(req)
-        html = response.read()
-        response.close()
-    except urllib2.HTTPError:
-        response = False
-        html = False
-    return html
+	try:
+		response = urllib2.urlopen(req)
+		html = response.read()
+		response.close()
+	except urllib2.HTTPError:
+		response = False
+		html = False
+	return html
 
 def get_params():
-    param = []
-    paramstring = sys.argv[2]
-    if len(paramstring) >= 2:
-        params = sys.argv[2]
-        cleanedparams = params.replace('?', '')
-        if (params[len(params) - 1] == '/'):
-            params = params[0:len(params) - 2]
-        pairsofparams = cleanedparams.split('&')
-        param = {}
-        for i in range(len(pairsofparams)):
-            splitparams = {}
-            splitparams = pairsofparams[i].split('=')
-            if (len(splitparams)) == 2:
-                param[splitparams[0]] = splitparams[1]
+	param = []
+	paramstring = sys.argv[2]
+	if len(paramstring) >= 2:
+		params = sys.argv[2]
+		cleanedparams = params.replace('?', '')
+		if (params[len(params) - 1] == '/'):
+			params = params[0:len(params) - 2]
+		pairsofparams = cleanedparams.split('&')
+		param = {}
+		for i in range(len(pairsofparams)):
+			splitparams = {}
+			splitparams = pairsofparams[i].split('=')
+			if (len(splitparams)) == 2:
+				param[splitparams[0]] = splitparams[1]
 
-    return param
+	return param
 
 def addListItem(label, image, url, isFolder, infoLabels = False, fanart = False, duration = False):
 	listitem = xbmcgui.ListItem(label = label, iconImage = image, thumbnailImage = image)
@@ -170,72 +170,72 @@ def addListItem(label, image, url, isFolder, infoLabels = False, fanart = False,
 	return ok
 
 def addLink(name, url, mode, iconimage, fanart=False, infoLabels=True):
-    u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode) + "&name=" + urllib.quote_plus(name) + "&iconimage=" + urllib.quote_plus(iconimage)
-    ok = True
-    liz = xbmcgui.ListItem(name, iconImage=iconimage, thumbnailImage=iconimage)
-    liz.setInfo(type="Video", infoLabels={"Title": name})
-    liz.setProperty('IsPlayable', 'true')
-    if not fanart:
-        fanart=defaultfanart
-    liz.setProperty('fanart_image',fanart)
-    ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz,isFolder=False)
-    return ok
+	u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode) + "&name=" + urllib.quote_plus(name) + "&iconimage=" + urllib.quote_plus(iconimage)
+	ok = True
+	liz = xbmcgui.ListItem(name, iconImage=iconimage, thumbnailImage=iconimage)
+	liz.setInfo(type="Video", infoLabels={"Title": name})
+	liz.setProperty('IsPlayable', 'true')
+	if not fanart:
+		fanart=defaultfanart
+	liz.setProperty('fanart_image',fanart)
+	ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz,isFolder=False)
+	return ok
 
 def add_item( action="" , title="" , plot="" , url="" ,thumbnail="" , folder=True ):
-    _log("add_item action=["+action+"] title=["+title+"] url=["+url+"] thumbnail=["+thumbnail+"] folder=["+str(folder)+"]")
+	_log("add_item action=["+action+"] title=["+title+"] url=["+url+"] thumbnail=["+thumbnail+"] folder=["+str(folder)+"]")
 
-    listitem = xbmcgui.ListItem( title, iconImage=iconimage, thumbnailImage=iconimage )
-    listitem.setInfo( "video", { "Title" : title, "FileName" : title, "Plot" : plot } )
-    
-    if url.startswith("plugin://"):
-        itemurl = url
-        listitem.setProperty('IsPlayable', 'true')
-        xbmcplugin.addDirectoryItem( handle=int(sys.argv[1]), url=itemurl, listitem=listitem)
-    else:
-        itemurl = '%s?action=%s&title=%s&url=%s&thumbnail=%s&plot=%s' % ( sys.argv[ 0 ] , action , urllib.quote_plus( title ) , urllib.quote_plus(url) , urllib.quote_plus( thumbnail ) , urllib.quote_plus( plot ))
-        xbmcplugin.addDirectoryItem( handle=int(sys.argv[1]), url=itemurl, listitem=listitem, isFolder=folder)
-        return ok
+	listitem = xbmcgui.ListItem( title, iconImage=iconimage, thumbnailImage=iconimage )
+	listitem.setInfo( "video", { "Title" : title, "FileName" : title, "Plot" : plot } )
+
+	if url.startswith("plugin://"):
+		itemurl = url
+		listitem.setProperty('IsPlayable', 'true')
+		xbmcplugin.addDirectoryItem( handle=int(sys.argv[1]), url=itemurl, listitem=listitem)
+	else:
+		itemurl = '%s?action=%s&title=%s&url=%s&thumbnail=%s&plot=%s' % ( sys.argv[ 0 ] , action , urllib.quote_plus( title ) , urllib.quote_plus(url) , urllib.quote_plus( thumbnail ) , urllib.quote_plus( plot ))
+		xbmcplugin.addDirectoryItem( handle=int(sys.argv[1]), url=itemurl, listitem=listitem, isFolder=folder)
+		return ok
 
 def addDir(name, url, mode, iconimage, fanart=False, infoLabels=True):
-    u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode) + "&name=" + urllib.quote_plus(name) + "&iconimage=" + urllib.quote_plus(iconimage)
-    ok = True
-    liz = xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-    liz.setInfo(type="Video", infoLabels={"Title": name})
-    liz.setProperty('IsPlayable', 'true')
-    if not fanart:
-        fanart=defaultfanart
-    liz.setProperty('fanart_image',fanart)
-    ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=True)
-    return ok
+	u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode) + "&name=" + urllib.quote_plus(name) + "&iconimage=" + urllib.quote_plus(iconimage)
+	ok = True
+	liz = xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+	liz.setInfo(type="Video", infoLabels={"Title": name})
+	liz.setProperty('IsPlayable', 'true')
+	if not fanart:
+		fanart=defaultfanart
+	liz.setProperty('fanart_image',fanart)
+	ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=True)
+	return ok
 
 
 def addDir2(name,url,mode,iconimage, fanart=False, infoLabels=False):
-        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
-        ok=True
-        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name } )
-        if not fanart:
-            fanart=defaultfanart
-        liz.setProperty('fanart_image',fanart)
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
-        return ok
+		u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+		ok=True
+		liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+		liz.setInfo( type="Video", infoLabels={ "Title": name } )
+		if not fanart:
+			fanart=defaultfanart
+		liz.setProperty('fanart_image',fanart)
+		ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+		return ok
 
 
 def addDirectoryItem2(name, isFolder=True, parameters={}):
-    ''' Add a list item to the XBMC UI.'''
-    li = xbmcgui.ListItem(name, iconImage=defaultimage, thumbnailImage=defaultimage)
-    li.setProperty('fanart_image', defaultfanart)
-    url = sys.argv[0] + '?' + urllib.urlencode(parameters)
-    return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=li, isFolder=isFolder)
+	''' Add a list item to the XBMC UI.'''
+	li = xbmcgui.ListItem(name, iconImage=defaultimage, thumbnailImage=defaultimage)
+	li.setProperty('fanart_image', defaultfanart)
+	url = sys.argv[0] + '?' + urllib.urlencode(parameters)
+	return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=li, isFolder=isFolder)
 
 
 def unescape(s):
-    p = htmllib.HTMLParser(None)
-    p.save_bgn()
-    p.feed(s)
-    return p.save_end()
+	p = htmllib.HTMLParser(None)
+	p.save_bgn()
+	p.feed(s)
+	return p.save_end()
 
-	
+
 
 
 params = get_params()
@@ -246,36 +246,42 @@ cookie = None
 iconimage = None
 
 try:
-    url = urllib.unquote_plus(params["url"])
+	url = urllib.unquote_plus(params["url"])
 except:
-    pass
+	pass
 try:
-    name = urllib.unquote_plus(params["name"])
+	name = urllib.unquote_plus(params["name"])
 except:
-    pass
+	pass
 try:
-    iconimage = urllib.unquote_plus(params["iconimage"])
+	iconimage = urllib.unquote_plus(params["iconimage"])
 except:
-    pass
+	pass
 try:
-    mode = int(params["mode"])
+	mode = int(params["mode"])
 except:
-    pass
+	pass
 
 print "Mode: " + str(mode)
 print "URL: " + str(url)
 print "Name: " + str(name)
 
 if mode == None or url == None or len(url) < 1:
-    print "Generate Smithsonian Magazine Main Menu"
-    index()
+	print "Generate Smithsonian Magazine Main Menu"
+	index()
 elif mode == 4:
-    print "Play Video"
+	print "Play Video"
 elif mode==633:
-        print "Smithsonian Magazine"
+	print "Smithsonian Magazine"
 	index()
 elif mode==636:
-        print "Smithsonian Magazine Videos"
+	print "Smithsonian Magazine Videos"
 	videos(url)
+elif mode==637:
+	print "Smithsonian Air & Space Streams"
+	sc_streams(url)
+elif mode==638:
+	print "Smithsonian Air & Space Streams"
+	streams(url)
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
