75e40c0b54462f1fb58766c31a569e41a5a3cf30
MetalChris <cpesce@gmx.com>
2018-06-11 19:07:17 -0500

    Update CFL Video
    Updates for 2018 season

--- plugin.video.cfl/addon.py
+++ plugin.video.cfl/addon.py
@@ -4,10 +4,9 @@
 # Written by MetalChris
 # Released under GPL(v2 or later)
 
-import urllib, urllib2, xbmcplugin, xbmcaddon, xbmcgui, htmllib, re, xbmcplugin, sys
+import urllib, urllib2, xbmcplugin, xbmcaddon, xbmcgui, htmllib, re, sys
 from bs4 import BeautifulSoup
 import html5lib
-import simplejson as json
 
 artbase = 'special://home/addons/plugin.video.cfl/resources/media/'
 _addon = xbmcaddon.Addon()
@@ -32,38 +31,17 @@ pubId = '4401740954001'
 local_string = xbmcaddon.Addon(id='plugin.video.cfl').getLocalizedString
 pluginhandle = int(sys.argv[1])
 confluence_views = [500,501,502,503,504,508,515]
+baseurl = 'http://www.cfl.ca/videos'
 
 
-def cfl(url):
-		addDir('Latest', 'http://www.cfl.ca/videos/latest-news/', 53, artbase + 'cfl.jpg')
-		addDir('Game Previews', 'http://www.cfl.ca/videos/game-previews/', 53, artbase + 'cfl.jpg')
-		addDir('2016 Game Recaps', 'http://www.cfl.ca/videos/2016-game-recaps/', 53, artbase + 'cfl.jpg')
-		addDir('Analysis', 'http://www.cfl.ca/videos/analysis/', 53, artbase + 'cfl.jpg')
-		addDir('Highlights', 'http://www.cfl.ca/videos/highlights/', 53, artbase + 'cfl.jpg')
-		addDir('Kegsize Plays of the Week', 'http://www.cfl.ca/videos/kegsize-plays-of-the-week/', 53, artbase + 'cfl.jpg')
-		addDir('103rd Grey Cup', 'http://www.cfl.ca/videos/gc103/', 53, artbase + 'cfl.jpg')
-		addDir('2015 Game Recaps', 'http://www.cfl.ca/videos/recaps/', 53, artbase + 'cfl.jpg')
-
-		xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-#10
-def stream(url):
-	html = get_html(url)
-	videoId = re.compile('iframe-(.+?)"').findall(html)
-	print videoId[0]
-	data = get_json('https://edge.api.brightcove.com/playback/v1/accounts/4401740954001/videos/' + str(videoId[0]))
-	jgdata = json.loads(data)
-	stream = 'http://c.brightcove.com/services/mobile/streaming/index/master.m3u8?videoId=' + str(videoId[0]) + '&pubId=4401740954001'
-	description = jgdata['description']
-	title = jgdata['name']
-	image = jgdata['poster']
-	print title
-	print description
-	print image
-	print stream
-	listitem = xbmcgui.ListItem(title, thumbnailImage=image)
-	xbmc.Player().play( stream, listitem )
-	sys.exit("Stop Video")
+def cfl(baseurl):
+	html = get_html('http://www.cfl.ca/videos')
+	soup = BeautifulSoup(html,'html5lib').find_all("ul",{"id":"dropdown-filter-featured"})[0]
+	for anchor in soup.find_all('a'):
+		title = anchor.text
+		url = 'http://www.cfl.ca/videos/' + anchor.get('href')
+		addDir(title, url, 53, artbase + 'cfl.jpg')
+	xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 
 #53
@@ -76,17 +54,37 @@ def cfl_2016(url):
 		url = h2.find('a')['href']
 		image = item.find("div",{"class":"card-image"})
 		image = ((re.compile('(\'(.+?)\')').findall(str(image))[0])[0]).replace("'","")
-		keys = image.split('_')
-		key = (keys[-1].split('-'))[0]
-		if len(key) != 13:
-			continue
-		stream = 'http://c.brightcove.com/services/mobile/streaming/index/master.m3u8?videoId=' + key + '&pubId=4401740954001'
-		#duration = striphtml(str(item.find('span', {'class':'time-bar'})))
-		#duration = int(get_sec(duration))
-		#print duration
-		#title.addStreamInfo('video', { 'duration': duration })
-		add_directory2(title, stream, 99, image, image, plot='')
-	xbmcplugin.setContent(pluginhandle, 'episodes')
+		url = 'plugin://plugin.video.cfl?mode=55&url=' + urllib.quote_plus(url)
+		li = xbmcgui.ListItem(title)
+		li.setProperty('IsPlayable', 'true')
+		li.setInfo(type="Video", infoLabels={"mediatype":"video","label":title,"title":title,"genre":"Sports"})
+		li.setArt({'thumb':image,'fanart':defaultfanart})
+		xbmcplugin.addDirectoryItem(handle=pluginhandle, url=url, listitem=li, isFolder=False)
+	xbmcplugin.endOfDirectory(pluginhandle, cacheToDisc=True)
+
+
+#55
+def get_stream(url):
+	xbmc.log('URL: ' + str(url))
+	html = get_html(url)
+	soup = BeautifulSoup(html,'html5lib').find_all("iframe",{"class":"brightcove-iframe"})[0]
+	xbmc.log('SOUP: ' + str(soup))
+	key = re.compile('videoId=(.+?)&amp;').findall(str(soup))[0]
+	xbmc.log('KEY: '  + str(key))
+	stream = 'http://c.brightcove.com/services/mobile/streaming/index/master.m3u8?videoId=' + key + '&pubId=4401740954001'
+	xbmc.log('STREAM: ' + str(stream))
+	PLAY(stream)
+	xbmcplugin.endOfDirectory(pluginhandle, cacheToDisc=True)
+
+
+#99
+def PLAY(url):
+	listitem = xbmcgui.ListItem(path=url)
+	xbmc.log('### SETRESOLVEDURL ###')
+	listitem.setProperty('IsPlayable', 'true')
+	xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, listitem)
+	xbmc.log('URL: ' + str(url), level=xbmc.LOGDEBUG)
+	xbmcplugin.endOfDirectory(pluginhandle)
 
 
 def get_sec(duration):
@@ -99,9 +97,6 @@ def play(name,url):
 	listitem = xbmcgui.ListItem(name, thumbnailImage=defaultimage)
 	xbmc.Player().play( url, listitem )
 	sys.exit("Stop Video")
-	#item = xbmcgui.ListItem(path=url)
-	#item.setProperty('IsPlayable', 'true')
-	#return xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
 
 
 def striphtml(data):
@@ -110,16 +105,16 @@ def striphtml(data):
 
 
 def add_directory2(name,url,mode,fanart,thumbnail,plot):
-		u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
-		ok=True
-		liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=thumbnail)
-		liz.setInfo( type="Video", infoLabels={ "Title": name,
-												"plot": plot} )
-		if not fanart:
-			fanart=''
-		liz.setProperty('fanart_image',fanart)
-		ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False, totalItems=10)
-		return ok
+	u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+	ok=True
+	liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=thumbnail)
+	liz.setInfo( type="Video", infoLabels={ "Title": name,
+											"plot": plot} )
+	if not fanart:
+		fanart=''
+	liz.setProperty('fanart_image',fanart)
+	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False, totalItems=10)
+	return ok
 
 def get_html(url):
 	req = urllib2.Request(url)
@@ -134,21 +129,6 @@ def get_html(url):
 		response = False
 		html = False
 	return html
-
-def get_json(url):
-	req = urllib2.Request(url)
-	req.add_header('Host', 'edge.api.brightcove.com')
-	req.add_header('User-Agent','Roku/DVP-4.3 (024.03E01057A), Mozilla/5.0 (iPad; CPU OS 8_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12B410 Safari/600.1.4')
-	req.add_header('Accept', 'application/json;pk=BCpkADawqM0dhxjC63Ux5MXyiMyIYB1S1bvk0iorISSaD1jFgWDyiv-JAcvE6XduNdDYxMdk_NTQWn91IQI9NLPkXd5UIw3cv49pcyJ5eW9QT0CWTrclSFHBHqSSyJ_9Ysgzc2v-Mw0wxNmZ')
-
-	try:
-		response = urllib2.urlopen(req)
-		json = response.read()
-		response.close()
-	except urllib2.HTTPError:
-		response = False
-		json = False
-	return json
 
 
 
@@ -232,5 +212,8 @@ elif mode == 10:
 elif mode == 53:
 	print "CFL 2016"
 	cfl_2016(url)
+elif mode == 55:
+	print "CFL Get Stream"
+	get_stream(url)
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
